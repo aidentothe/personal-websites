8c2219d5cef06e6a8d99dac36a0e6dd018b035ef
Aiden Huang <huang.a.aiden@gmail.com>
2025-05-09 02:09:16 -0700

    changed to triangles ya. heard

--- components/BootScreen.tsx
+++ components/BootScreen.tsx
@@ -16,8 +16,16 @@ export default function BootScreen() {
         transition={{ delay: 0.5, duration: 0.8 }}
         className="mb-8"
       >
-        <div className="h-24 w-24 rounded-full border-4 border-blue-500 flex items-center justify-center">
-          <span className="text-4xl font-bold text-blue-500">AH</span>
+        <div className="h-24 w-24 rounded-full border-4 border-blue-500 flex items-center justify-center bg-black">
+          {/* Stylized logo: vertical trapezoid with circle below, matching user image */}
+          <svg width="60" height="70" viewBox="0 0 60 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+            {/* Trapezoid (like an 'A' without crossbar) */}
+            <polygon points="10,60 30,10 50,60" fill="#bbb" />
+            {/* Rectangle for the vertical bar */}
+            <rect x="24" y="30" width="12" height="30" fill="#bbb" />
+            {/* Circle below the bar */}
+            <circle cx="30" cy="65" r="6" fill="#bbb" />
+          </svg>
         </div>
       </motion.div>
 
