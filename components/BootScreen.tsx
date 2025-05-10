e8a6d9f0520e3e9c5507616da4f3c632ee564c5b
Aiden Huang <huang.a.aiden@gmail.com>
2025-05-10 00:12:03 -0700

    changed to triangles ya. heard

--- components/BootScreen.tsx
+++ components/BootScreen.tsx
@@ -17,14 +17,14 @@ export default function BootScreen() {
         className="mb-8"
       >
         <div className="h-24 w-24 rounded-full border-4 border-blue-500 flex items-center justify-center bg-black">
-          {/* Stylized logo: vertical trapezoid with circle below, matching user image */}
-          <svg width="60" height="70" viewBox="0 0 60 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-            {/* Trapezoid (like an 'A' without crossbar) */}
-            <polygon points="10,60 30,10 50,60" fill="#bbb" />
-            {/* Rectangle for the vertical bar */}
-            <rect x="24" y="30" width="12" height="30" fill="#bbb" />
-            {/* Circle below the bar */}
-            <circle cx="30" cy="65" r="6" fill="#bbb" />
+          {/* Stylized logo: two thick white bars forming an 'A' with a circle below, matching the provided image */}
+          <svg width="90" height="90" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg">
+            {/* Left bar */}
+            <rect x="17" y="15" width="14" height="50" rx="2" fill="#fff" transform="rotate(-10 17 15)" />
+            {/* Right bar */}
+            <rect x="59" y="15" width="14" height="50" rx="2" fill="#fff" transform="rotate(10 59 15)" />
+            {/* Circle */}
+            <circle cx="45" cy="73" r="12" fill="#fff" />
           </svg>
         </div>
       </motion.div>
